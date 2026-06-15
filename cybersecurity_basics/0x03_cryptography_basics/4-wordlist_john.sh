a557af761d00df694043a2eb3506e8a11193bb1b
Simeon <smashev@gmail.com>
2026-06-15 22:36:06 +0200

    task 4 attempt 2

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=Raw-MD5
-john --show "$1" --format=Raw-MD5 | awk -F: '{print $NF}' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=Raw-MD5 > 4-password.txt
