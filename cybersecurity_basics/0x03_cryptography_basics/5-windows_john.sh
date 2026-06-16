04a133352aab7939e3f70fe2313251a9130f9af4
Simeon <smashev@gmail.com>
2026-06-16 19:53:29 +0200

    task 5

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=NT > 5-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=NT > /dev/null 2>&1 && john --show "$1" --format=NT | awk -F: '{print $NF}' | head -1 > 5-password.txt
