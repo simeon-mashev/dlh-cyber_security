e9833c913e04675b012a201fd117a9e233f2523e
Simeon <smashev@gmail.com>
2026-07-04 13:00:01 +0200

    task 5 4

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent -active -oI > "$1.txt" && subfinder -d $1 -silent
+subfinder -d "$1" -silent -active -oI | awk -F, '{print $1","$2}' > "$1.txt" && subfinder -d "$1" -silent
