37657dffc5661a2f37ace49bffbe94bdfe3fd3b5
Simeon <smashev@gmail.com>
2026-07-04 10:36:08 +0200

    task 4 attempt

--- network_security/0x01_passive_reconnaissance/4-dig_all.sh
+++ network_security/0x01_passive_reconnaissance/4-dig_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dig "$1" A +noall +answer && dig "$1" AAAA +noall +answer && dig "$1" MX +noall +answer && dig "$1" NS +noall +answer && dig "$1" TXT +noall +answer
+dig "$1" A +noall +answer && dig "$1" MX +noall +answer && dig "$1" NS +noall +answer && dig "$1" TXT +noall +answer
