5adb1c6b7a726b825b9325380246c49b9ad1eb88
Simeon <smashev@gmail.com>
2026-07-04 10:34:14 +0200

    task 4 try 2

--- network_security/0x01_passive_reconnaissance/4-dig_all.sh
+++ network_security/0x01_passive_reconnaissance/4-dig_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dig +noall +answer $1 ANY
+dig "$1" A +noall +answer && dig "$1" AAAA +noall +answer && dig "$1" MX +noall +answer && dig "$1" NS +noall +answer && dig "$1" TXT +noall +answer
