1f7c883343d16455700a2a447b547d99432083f1
Simeon <smashev@gmail.com>
2026-07-08 22:16:08 +0200

    task4 exceptions

--- scripting_cyber/0x01-python_scripting/3-http_headers.py
+++ scripting_cyber/0x01-python_scripting/3-http_headers.py
@@ -3,9 +3,12 @@ import sys
 import requests
 
 def get_http_headers(url):
-    page = requests.get(url)
-    output = {'status_code': page.status_code, 'headers': dict(page.headers) }
-    return output
+    try:
+        page = requests.get(url)
+        output = {'status_code': page.status_code, 'headers': dict(page.headers) }
+        return output
+    except requests.exceptions.RequestException:
+        return None
 
 
 
